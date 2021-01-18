c726c168b1bc433b2f282a75644ab5e376f91bf1
perezcarlos93 <perez.carlos_a@yahoo.com>
2021-01-17 20:01:23 -0800

    added additional routes for future development

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,29 @@
 const router = require('express').Router();
 
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
   try {
     res.render('homepage');
+  } catch (err) {
+    // res.json.status(500);
+  }
+});
+router.get('/aboutme', (req, res) => {
+  try {
+    res.render('aboutme');
+  } catch (err) {
+    // res.json.status(500);
+  }
+});
+router.get('/projects', (req, res) => {
+  try {
+    res.render('projects');
+  } catch (err) {
+    // res.json.status(500);
+  }
+});
+router.get('/contactme', (req, res) => {
+  try {
+    res.render('contact');
   } catch (err) {
     // res.json.status(500);
   }
